42731a0535121051815a7d592ee29fd230bd1022
Mohammad Minhaj <minhajrafeeq7@gmail.com>
2025-11-10 13:13:54 +0530

    Created all routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
-function App() {
+const GreetingPage = ({ name, message }) => (
+  <main className="page">
+    <h1>Hello {name}!</h1>
+    <p>{message}</p>
+  </main>
+)
 
+function App() {
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route
+        path="/haytham"
+        element={<GreetingPage name="Haytham" message="Welcome to your dedicated page." />}
+      />
+      <Route
+        path="/ola"
+        element={<GreetingPage name="Ola" message="Hope you have a great day!" />}
+      />
+      <Route
+        path="/sabah"
+        element={<GreetingPage name="Sabah" message="Wishing you a bright morning!" />}
+      />
+    </Routes>
   )
 }
 
